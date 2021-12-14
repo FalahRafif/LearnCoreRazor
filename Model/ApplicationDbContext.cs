c803fb08ebf7127189a3e548c3eecb3c8164d439
Falah Rafif <falah.rfif@gmail.com>
2021-12-14 14:20:37 +0700

    create file ApplicationDbContext.cs in Model/ and config startup for set dbcontext to pipeline also add migration

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext //inherit with EF DbContext
+    {
+        //add-migration generate script sql
+        //update-database generate/update database
+        //pass base > options
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        //entry
+        public DbSet<Book> Book { get; set; }
+    }
+}
