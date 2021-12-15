31b781f058430fd2da6cbec57626167dbf2b26d5
Falah Rafif <falah.rfif@gmail.com>
2021-12-15 11:18:05 +0700

    Add Logic and validation in Pages/BookList/Create.cshtml + .cs

--- Pages/BookList/Create.cshtml.cs
+++ Pages/BookList/Create.cshtml.cs
@@ -17,9 +17,25 @@ namespace BookListRazor.Pages.BookList
         {
             _db = db;
         }
+        [BindProperty] //on post getting book on post handler
         public Book Book { get; set; }
         public void OnGet()
         {
+
+        }
+
+        public async Task<IActionResult> OnPost() //handle to retreive data | Iactionresult redirect to main page
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book); //add data to queue
+                await _db.SaveChangesAsync(); //push data to db
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
